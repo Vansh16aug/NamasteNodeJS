eebcff7db71b236fb466a180c0f7c74277063ff3
Vansh16aug <kumarvansh16aug@gmail.com>
2024-08-15 19:56:52 +0530

    ep-4 Namaste NodeJS

---
+++ Ep-4/app.js
@@ -0,0 +1,14 @@
+console.log("Inside appjs");
+
+// cjs
+// function add(a, b) {
+//     console.log(a+b);
+// }
+
+// mjs
+export function add(a, b) {
+  console.log(a + b);
+}
+
+//cjs
+// module.exports = add;
---
+++ Ep-4/index.js
@@ -0,0 +1,9 @@
+// cjs
+// const add = require("./app");
+
+// mjs
+import { add } from "./app.js";
+
+console.log("Inside indexjs");
+
+add(1, 2); // 3
