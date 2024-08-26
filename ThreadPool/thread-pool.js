04957e3ad9ec76cc4af8e9a3627f2884d880085a
Vansh16aug <kumarvansh16aug@gmail.com>
2024-08-26 11:43:52 +0530

    update: learnt thread-pool

---
+++ ThreadPool/thread-pool.js
@@ -0,0 +1,21 @@
+const fs = require("fs");
+const crypto = require("crypto");
+
+crypto.pbkdf2("password","salt",500000,50,"sha512",(err,key)=>{
+    console.log("1 - crypto done");
+});
+// fs.readFile('file.txt',"utf8",()=>{
+//     console.log("fileRead Done");
+// })
+crypto.pbkdf2("password", "salt", 500000, 50, "sha512", (err, key) => {
+  console.log("2 - crypto done");
+});
+crypto.pbkdf2("password", "salt", 500000, 50, "sha512", (err, key) => {
+  console.log("3 - crypto done");
+});
+crypto.pbkdf2("password", "salt", 500000, 50, "sha512", (err, key) => {
+  console.log("4 - crypto done");
+});
+crypto.pbkdf2("password", "salt", 500000, 50, "sha512", (err, key) => {
+  console.log("5 - crypto done");
+});
